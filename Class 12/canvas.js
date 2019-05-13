7a5ee90efeb8bb56adfe9d1e5a5dfdbb0a553949
Gizelle Hurtado <gizelle_hurtado@yahoo.com>
2019-05-12 20:59:54 -0700

    Code done in class

---
+++ Class 12/canvas.js
@@ -0,0 +1,26 @@
+var canvas = document.getElementById('Game');
+var context = canvas.creatContext('2d');
+
+var ballX = canvas.width/2;
+var ballY = canvas.hight/2;
+var ballColor = 'rgb(0,155,155)';
+
+var ballRadius = 50;
+
+var speedX = 5;
+var speedY = 3;
+var moveRight = true;
+var moveDown = true;
+
+var animate = function () {
+
+  context.fillStyle = 'rgba(255, 255, 255, 0.6)';
+  context.fillRect'rgba(0, 0, canvas.width, canvas.hight)';
+
+  context.fillStyle = 
+
+
+   window.requestAnimationFrame ('animate')
+}
+
+animate ();
