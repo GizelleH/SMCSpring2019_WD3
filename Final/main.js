954079b6690072b46784d29cd1eab591672756a5
Gizelle Hurtado <gizelle_hurtado@yahoo.com>
2019-06-03 23:53:47 -0700

    Adding to final website
    Added animation to images on home page, added another carousel, added a checkout page, updated contact page, added different shoping page views, added cart and add to cart buttons that update carts number, ect..

--- Final/main.js
+++ Final/main.js
@@ -2,3 +2,56 @@
     $('#recipeCarousel').carousel({
         interval: 2500
     });
+
+
+
+    
+      
+
+       
+
+
+
+
+
+
+$(document).ready(function() {
+
+ 
+  // If Mobile, add background color when toggler is clicked
+  $(".navbar-toggler").click(function() {
+    if (!$(".navbar-collapse").hasClass("show")) {
+      $(".navbar").addClass("bg-dark");
+    } else {
+      if ($(window).scrollTop() < 56) {
+        $(".navbar").removeClass("bg-dark");
+      } else {
+      }
+    }
+  });
+
+  $('#addCart').click(function(e){
+                e.preventDefault();
+                var acceptableValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+                var count = $('#Count').val();
+                var currentCount = $('.cart-count').text();
+                if(currentCount !== ""){
+                    currentCount = parseInt(currentCount);
+                }
+                else {
+                    currentCount = 0;
+                }
+                if(count !== ""){
+                    console.log(parseInt(currentCount));
+                    console.log(parseInt(count));
+                    $('.cart-count').text(parseInt(currentCount)+parseInt(count));
+                }
+            })
+
+});
+
+
+
+
+
+  
